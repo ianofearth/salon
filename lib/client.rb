4ead1f4aae545ab1060a8c31f513418cfb25c4ab
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-08 09:39:20 -0700

    add spec to test #client_name method

--- lib/client.rb
+++ lib/client.rb
@@ -6,5 +6,17 @@ class Client
 		@stylist_name = attributes.fetch(:stylist_id)
 	end
 
+	
+
+	# define_singleton_method(:all) do
+	# 	returned_clients = DB.exec("SELECT * FROM clients")
+	# 	clients = []
+	# 	returned_clients.each() do |client|
+	# 		@client_name = client.fetch("client_name")
+	# 		stylist_id = client.fetch("stylist_id").to_i()
+	# 		clients.push(Client.new({:client_name => client_name, :stylist_id => stylist_id}))
+	# 	end
+	# end
+
 
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,12 +1,19 @@
 require("spec_helper")
 
 describe(Client) do
-	
-	describe(".all") do
-		it("is empty at first") do
-			expect(Client.all()).to(eq([]))
+
+	describe("#client_name") do
+		it("lets you enter a client") do
+			test_client = Client.new({:client_name => "ian", :stylist_id => 1})
+			expect(test_client.client_name()).to(eq("ian"))
 		end
 	end
+	
+	# describe(".all") do
+	# 	it("is empty at first") do
+	# 		expect(Client.all()).to(eq([]))
+	# 	end
+	# end
 
 	
 end
