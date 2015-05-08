ae1be612887eac7805e5d2d630fdc01f663bfcb5
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-08 12:36:55 -0700

    add content to app.rb, stylists.erb

---
+++ app.rb
@@ -0,0 +1,17 @@
+require("sinatra")
+require("sinatra/reloader")
+require("./lib/client")
+require("./lib/stylist")
+also_reload("lib/**/*.rb")
+require("pg")
+
+DB = PG.connect({:dbname => "salon"})
+
+get("/") do
+	erb(:index)
+end
+
+get("/stylists") do
+	@stylists = Stylists.all()
+	erb(:stylists)
+end
