0f36273b3b89df9a831e611a2f29daa4800ef9f7
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-08 10:13:12 -0700

    add spec to test #== method

--- lib/client.rb
+++ lib/client.rb
@@ -1,8 +1,8 @@
 class Client
-	attr_reader(:client, :stylist_id)
+	attr_reader(:client_name, :stylist_id)
 
 	define_method(:initialize) do |attributes|
-		@client = attributes.fetch(:client)
+		@client_name = attributes.fetch(:client_name)
 		@stylist_id = attributes.fetch(:stylist_id)
 	end
 
@@ -12,9 +12,9 @@ class Client
 	# 	returned_clients = DB.exec("SELECT * FROM clients")
 	# 	clients = []
 	# 	returned_clients.each() do |client|
-	# 		@client = client.fetch("client")
+	# 		@client_name = client.fetch("client")
 	# 		stylist_id = client.fetch("stylist_id").to_i()
-	# 		clients.push(Client.new({:client => client, :stylist_id => stylist_id}))
+	# 		clients.push(Client.new({:client_name => client_name, :stylist_id => stylist_id}))
 	# 	end
 	# end
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,17 +2,25 @@ require("spec_helper")
 
 describe(Client) do
 
-	describe("#client") do
+	describe("#client_name") do
 		it("lets you enter a client") do
-			test_client = Client.new({:client => "ian", :stylist_id => 1})
-			expect(test_client.client()).to(eq("ian"))
+			test_client = Client.new({:client_name => "ian", :stylist_id => 1})
+			expect(test_client.client_name()).to(eq("ian"))
 		end
 	end
 
-	describe("stylist_id") do
+	describe("#stylist_id") do
 		it("lets you read out the stylist ID") do
-			test_client = Client.new({:client => "ian", :stylist_id => 1})
+			test_client = Client.new({:client_name => "ian", :stylist_id => 1})
 			expect(test_client.stylist_id()).to(eq(1))
+		end
+	end
+
+	describe("#==") do
+		it("is the same client if it has the same client_name") do
+			client = Client.new({:client_name => "spiderman", :stylist_id => 1})
+			client2 = Client.new({:client_name => "spiderman", :stylist_id => 1})
+			expect(client).to(eq(client2))
 		end
 	end
 	
