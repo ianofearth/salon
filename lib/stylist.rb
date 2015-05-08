e6c079b89aa3495a0a0bdafe4c994a2341573638
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-08 11:41:12 -0700

    pass .find method spec

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -26,6 +26,16 @@ class Stylist
 		self.stylist_name().==(another_stylist.stylist_name()).&(self.id().==(another_stylist.id()))
 	end
 
+	define_singleton_method(:find) do |id|
+		found_stylist = nil
+		Stylist.all().each() do |stylist|
+			if stylist.id().==(id)
+				found_stylist = stylist
+			end
+		end
+		found_stylist
+	end
+
 
 
 
