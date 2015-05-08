927051cd72a9b348585328e730df4ba2b48fce4c
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-08 12:40:20 -0700

    add routes for adding new stylists

--- app.rb
+++ app.rb
@@ -15,3 +15,14 @@ get("/stylists") do
 	@stylists = Stylists.all()
 	erb(:stylists)
 end
+
+get("/stylists/new") do
+	erb(:stylist_form)
+end
+
+post("/stylists") do
+	stylist_name = params.fetch("stylist_name")
+	stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
+	stylist.save()
+	erb(:stylist_success)
+end
