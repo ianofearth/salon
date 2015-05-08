f4f906ca8c93037261ccf4ac0dc92ed148ecc335
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-05-08 10:01:57 -0700

    add spec to test #stylist_id method, pass #stylist method spec

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,6 +8,13 @@ describe(Client) do
 			expect(test_client.client()).to(eq("ian"))
 		end
 	end
+
+	describe("stylist_id") do
+		it("lets you read out the stylist ID") do
+			test_client = Client.new({:client => "ian", :stylist_id => 1})
+			expect(test_client.stylist_id()).to(eq(1))
+		end
+	end
 	
 	# describe(".all") do
 	# 	it("is empty at first") do
